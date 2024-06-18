c3ff7d299d4811e0830794e488d09687b5616ea7
sreeharipdsy <sreehari.pdsy@gmail.com>
2024-06-18 20:27:31 +0530

    aos-update

--- assets/js/accordion.js
+++ assets/js/accordion.js
@@ -1,30 +1,3 @@
-// var acc = document.getElementsByClassName("accordion");
-// var i;
-
-// for (i = 0; i < acc.length; i++) {
-//   acc[i].addEventListener("click", function() {
-//     this.classList.toggle("active");
-//     var panel = this.nextElementSibling;
-//     if (panel.style.maxHeight) {
-//       panel.style.maxHeight = null;
-//     } else {
-//       panel.style.maxHeight = panel.scrollHeight + "px";
-//     } 
-//   });
-// }
-
-// document.getElementById("target-me").open = true;
-// const details = document.querySelectorAll("details");
-// details.forEach((targetDetail) => {
-//   targetDetail.addEventListener("click", () => {
-//     details.forEach((detail) => {
-//       if (detail !== targetDetail) {
-//         detail.removeAttribute("open");
-//       }
-//     });
-//   });
-// });
-
 var acc = document.getElementsByClassName("accordion");
 var panel = document.getElementsByClassName('panel');
 
@@ -47,34 +20,3 @@ function setClass(els, className, fnName) {
     }
 }
 
-/*var acc = document.getElementsByClassName("accordion");
-var panel = document.getElementsByClassName('panel');
-
-for (var i = 0; i < acc.length; i++) {
-    acc[i].onclick = function() {
-        var setClasses = !this.classList.contains('active');
-        setClass(acc, 'active', 'remove');
-        setClass(panel, 'show', 'remove');
-
-        if (setClasses) {
-            this.classList.toggle("active");
-            this.nextElementSibling.classList.toggle("show");
-        }
-    }
-}
-
-function setClass(els, className, fnName) {
-    for (var i = 0; i < els.length; i++) {
-        els[i].classList[fnName](className);
-    }
-}
-
-$('.accordion').click(function(e) {
-	e.preventDefault();
-	var currentIsActive = $(this).hasClass('is-active');
-	$(this).parent('.content').find('> *').removeClass('is-active');
-	if(currentIsActive != 1) {
-		$(this).addClass('is-active');
-		$(this).next('.panel').addClass('is-active');
-	}
-});*/
